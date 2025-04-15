77c59e071a1dad29a396791568cade6810ae202a
LiviaSobral <134714074+LiviaSobral@users.noreply.github.com>
2025-04-15 17:10:47 -0300

    Add files via upload

---
+++ loginHash/src/main/java/com/mycompany/loginhash/LoginHash.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginhash;
+
+import com.mycompany.loginhash.dao.UsuarioDAO;
+import com.mycompany.loginhash.database.ConnectionSQL;
+import com.mycompany.loginhash.model.Usuario;
+
+/**
+ *
+ * @author Aluno
+ */
+public class LoginHash {
+
+    public static void main(String[] args) {
+        ConnectionSQL.conectar();
+        
+        Usuario usuario = new Usuario("mai1","senha123");
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        usuarioDAO.registrarUsuario(usuario);
+        usuarioDAO.validarLogin(usuario);
+        usuarioDAO.buscarUsuarioPorEmail(usuario.getEmail());
+        
+    }
+}
